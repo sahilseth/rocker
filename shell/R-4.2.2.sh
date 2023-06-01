1e051cfa57d14a4aed244a909dd29dd6441b442c
sahil seth <sahiilseth@gmail.com>
2023-06-01 13:00:24 -0400

    Create R-4.2.2.sh

---
+++ shell/R-4.2.2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+plat="debian"
+ver="4.2"
+tag="4.2.2"
+
+# User-installed R packages go into their home directory
+if [ ! -e ${HOME}/.Renviron ]
+then
+  printf '\nNOTE: creating ~/.Renviron file\n\n'
+  echo "R_LIBS_USER=~/R/${plat}/${ver}" >> ${HOME}/.Renviron
+fi
+
+if [[ `echo $HOSTNAME | grep "dst001"` ]]; then
+    docker run -ti -v $HOME:/home/seths3 \
+     --rm \
+     -v /stash:/stash -v /aws-storage:/aws-storage \
+    -v /:/host_root \
+    -u seths3 -w $(pwd) \
+    sahilseth/rocker_ml-verse:${tag} R "$@"
+fi
